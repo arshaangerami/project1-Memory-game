6ccded353cb3bf6419664ac036204e3690cc81e3
masinakhjiri <ms_nakhjiri@yahoo.com>
2022-04-29 23:48:05 -0700

    work on js- add flipCard fun

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-console.log('hello')
+//acsess to my html elements with querySelectorAll
+const gameCards = document.querySelectorAll('.game-card')
+console.log(gameCards)
+
+//Flip card to reveal the character image
+function flipBack(){
+    // console.log("hi")
+    // console.log(this)
+    this.classList.toggle('flip')
+
+}
+
+// Add eventListener to each card when I click on ech of them do flipBack function
+gameCards.forEach(card => card.addEventListener('click',flipBack))
