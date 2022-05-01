6e0a271d15068322ae4a847b93fbcdeec5b3ce41
masinakhjiri <ms_nakhjiri@yahoo.com>
2022-05-01 16:19:15 -0700

    add function for shuffleing cards each time

--- scripts.js
+++ scripts.js
@@ -1,14 +1,46 @@
-//acsess to my html elements with querySelectorAll
-const gameCards = document.querySelectorAll('.game-card')
-console.log(gameCards)
 
-//Flip card to reveal the character image
-function flipBack(){
-    // console.log("hi")
-    // console.log(this)
-    this.classList.toggle('flip')
+let imgNameList=['batman.png','black-widow.png','captain-america.png','hulk.png','spider-man.png','wonder-woman.png']
+//this function gets the list of image arrays and duplicate it 
+function getImageList(array){
+    let duplicateImageName = array.concat(array)
+//shuffle the cards randomly every time the game starts
+    duplicateImageName.sort(()=> Math.random() - .5)
+    return duplicateImageName
 
 }
 
+// function prepareCards(){
+
+// }
+window.addEventListener('load',() =>{
+    let playGameBtn =document.getElementById('play-game')
+    playGameBtn.onclick = () => {
+        alert(getImageList(imgNameList))
+        
+    }
+})
+//set an empty array to push card choices into it
+// playerChoice =[]
+
+//Flip card to reveal the character image
+// function flipBack(){
+//     // console.log("hi")
+//     // console.log(this)
+//     //with toggle if the class is there remove it if it's not added
+//     this.classList.toggle('flip')
+//     // if(playerChoice.length !== 2){
+//     //     //only clicked on one card
+//     //     playerChoice.push()
+        
+        
+//     // }
+    
+// }
+
 // Add eventListener to each card when I click on ech of them do flipBack function
-gameCards.forEach(card => card.addEventListener('click',flipBack))
+// gameCards.forEach(card => card.addEventListener('click',flipBack))
+// window.addEventListener('load' , ()=>{
+//     setTimeout()
+
+// })
+
