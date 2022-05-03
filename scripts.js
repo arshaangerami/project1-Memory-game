08da70f62a9f07d4f9b76b8f66e8f5abf0438f0a
masinakhjiri <ms_nakhjiri@yahoo.com>
2022-05-02 21:10:10 -0700

    add fun for making src and counting time

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 
 let imgNameList=['batman.png','black-widow.png','captain-america.png','hulk.png','spider-man.png','wonder-woman.png']
+
+let timer = 10
+let timerIntervalID;
+
 //this function gets the list of image arrays and duplicate it 
-function getImageList(array){
+function getImageNameList(array){
     let duplicateImageName = array.concat(array)
 //shuffle the cards randomly every time the game starts
     duplicateImageName.sort(()=> Math.random() - .5)
@@ -9,13 +13,35 @@ function getImageList(array){
 
 }
 
-// function prepareCards(){
+function prepareCards(){
+    //getElementbyClassname return a HTML Collection
+    let imageCards = document.getElementsByClassName('card-back')
+    let imageNames = getImageNameList(imgNameList)
+    //console.log(imageCards)
+    //array.from make a copy of array and I want to make a src for 12 cards
+    Array.from(imageCards).forEach((el) => {
+        let imageName = imageNames.pop()
+        el.src = `./img/${imageName}`
 
-// }
+    })
+ }
+//this function count down the timer
+ function timerCountDown(){
+     timer--;
+     document.getElementById('timer').innerHTML = timer
+     //we need to clearInterval to set time till 0 
+     if(timer === 0){
+         clearInterval(timerIntervalID)
+     }
+
+
+ }
 window.addEventListener('load',() =>{
     let playGameBtn =document.getElementById('play-game')
     playGameBtn.onclick = () => {
-        alert(getImageList(imgNameList))
+        prepareCards()
+        timerIntervalID = setInterval(timerCountDown,1000)
+        
         
     }
 })
