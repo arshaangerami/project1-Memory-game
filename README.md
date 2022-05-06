09e4ae454a182198086a564ff79fde1fce6f4862
masinakhjiri <ms_nakhjiri@yahoo.com>
2022-05-06 01:51:32 -0700

    add readme.md into vscode

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# project1-Memory-game
+# Memory-Matching-Game
+> Click on the cards to reveal a shape. If they are a match, they will stay at board if they aren't match they flip again. You have 60 seconds to clear the board! 
+
+# Screenshots
+
+<img src="img/Screen Shot 2022-05-06 at 12.06.01 AM.png" alt="memory-game" />
+<img src="img/Screen Shot 2022-05-06 at 12.06.48 AM.png" alt="memory-game" />
+
+
+# Installation Instructions
+# User stories
+## MVP Goals
+
+* As a player,I should be able to start new memory game.
+* As a player,I want to be able to click on cards sequentially to turn them over(flipping first card then second card and so on)
+* As a player,I can see the cards that have been correctly matched and counts of total moves.
+* As a player,once two cards matched correctly , they disappear from the interface.
+* As a player,I want to see the message when I win/lose.
+* As a player,I could not to continue playing when I win/lose.
+* As a player,I won't be able to play a game without resetting the game.
+* As a player,I want to view the result of a game.
+* As a player,all pairs have been matched correctly before the time is up then the player win.
+
+## Stretch Goals
+
+* As a player,I can start at level 1 and progress to level 5(each level is more difficult than the previous one).
+* As a player,I can choose between single player or two player.
+* As a player,when two cards matched, I want to listen the audio.
+# Wireframes
+<img src="img/2.png" alt="memory-game" />
+<img src="img/5.png" alt="memory-game" />
+<img src="img/9.png" alt="memory-game" />
+<img src="img/10.png" alt="memory-game" />
+
+
+# Technologies Used 
+- HTML
+- CSS
+- JAVASCRIPT
--- scripts.js
+++ scripts.js
@@ -1,24 +1,25 @@
 let imgNameList = ['batman.png', 'black-widow.png', 'captain-america.png', 'hulk.png', 'spider-man.png', 'wonder-woman.png']
-let timer = 10;
+let timer = 60;
 let timerIntervalID;
+//make a obj of array with 12 empty item in it
 let cardArray = new Array(12)
-let lastSelectedCard = null;
+let firstSelectedCard = null;
 let moveCount = 0;
   
 
-
+//flip all 12 cards
 function flipAllCards(){
     cardArray.forEach((object) => {
         object.card.classList.toggle('flip')
     })
 }
-
+//this function gets the list of image arrays and duplicate it and shuffle them
 function getImageNameList(array){
     let duplicateImgName = array.concat(array);
     duplicateImgName.sort(() => Math.random() - 0.5);
     return duplicateImgName;
 }
-
+//when two cards doesn't match
 function flipMissMatchCard(card1, card2){
     card1.card.classList.toggle('flip')
     card1.flipPremission = true
@@ -26,7 +27,7 @@ function flipMissMatchCard(card1, card2){
     card2.card.classList.toggle('flip')
     card2.flipPremission = true
 }
-
+//check the game is finished or not
 function isFinished(){
     for(let i=0; i<12; i++){
         if(cardArray[i].flipPremission === true)
@@ -34,20 +35,18 @@ function isFinished(){
     }
     return true
 }
-
-
-
+//make the board game and set attributes to it
 function prepareCards() {
 
     let cardContainer = document.getElementById('card-container')
+    //prevents make board game multiple time 
     cardContainer.innerHTML = "";
-
+    //This is the array with 12 cards and shuffled
     let imageNames = getImageNameList(imgNameList)
-
+    //Refactor my html with for loop and make the board game
     for (let i=0; i<12; i++){
 
         let imageName = imageNames.pop()
-
         let gameCard = document.createElement('div')
         gameCard.classList.add('game-card')
 
@@ -69,64 +68,64 @@ function prepareCards() {
             'flipPremission': true,
             'name': imageName
         }
-
-
+        //makes flip the cards
         gameCard.onclick = () => {
-            let presentCard = cardArray[i]
-            if(presentCard.flipPremission === true){
-                presentCard.card.classList.toggle('flip')
-                presentCard.flipPremission = false
-                if(lastSelectedCard !== null){
-                    if(lastSelectedCard.name === presentCard.name){
-                        lastSelectedCard = null;
-                        
-                        if(isFinished === true){
+            let currentCard = cardArray[i]
+            //set condition for when I click on card then I couldn't click on it again
+            if(currentCard.flipPremission === true){
+                currentCard.card.classList.toggle('flip')
+                //makes the card not flip again
+                currentCard.flipPremission = false
+                //we select two cards here so go for match them
+                if(firstSelectedCard !== null){
+                    if(firstSelectedCard.name === currentCard.name){
+                        //after match happen the third card will be the first card
+                        firstSelectedCard = null;
+                        //check finishing the game here,all card are matched
+                        if (isFinished() === true){
                             setTimeout(() => {
-                                alert('Victory')
-                                clearInterval(timerIntervalID)
-                            },500)
+                                alert('Victory!');
+                                clearInterval(timerIntervalID);
+                            }, 500)
                         }
+                    //when the two cards are not matched
                     }else{
-                        setTimeout(flipMissMatchCard, 1000, lastSelectedCard, presentCard)
-                        lastSelectedCard = null;
-
+                        setTimeout(flipMissMatchCard, 1000, firstSelectedCard, currentCard)
+                        firstSelectedCard = null;
                     }
-                }else{ 
+
+                }//here click on first card
+                else{
                     moveCount++;
-                    lastSelectedCard = presentCard
-                    
+                    firstSelectedCard = currentCard
                 }
+    
                 document.getElementById('move-number').innerHTML = moveCount
 
             }
             
-            
         }
     
     }
-
-
+    //when the bord cards make with for loop this func flip all cards and again after 3s it flip the cards again
     flipAllCards();
     setTimeout(flipAllCards, 3000);
 
-
-
-
 }
-
+//this function count down the timer
 function timerCountDown(){
     timer--;
     document.getElementById('timer').innerHTML = timer;
 
     if(timer === 0){
-        if(isFinished() === true){
-            alert('victory')
-        }else{
-            alert('Game Over')
-        }
+
+        if (isFinished() === true)
+            alert('Victory')
+        else
+            alert('Game over!')
 
         clearInterval(timerIntervalID);
-        ///// 
+        
     }
 }
 
@@ -134,171 +133,19 @@ window.addEventListener('load', () => {
     let playGameBtn = document.getElementById('play-game')
 
     playGameBtn.onclick = () => {
-        timer =10
-        moveCount = 0
+
+        timer = 60;
+        moveCount = 0;
         clearInterval(timerIntervalID)
-        document.getElementById('move-number').innerHTML = moveCount
-        document.getElementById('timer').innerHTML = timer
+        document.getElementById('move-number').innerHTML = moveCount;
+        document.getElementById('timer').innerHTML = timer;
+
         prepareCards()
 
         timerIntervalID = setInterval(timerCountDown, 1000);
     }
 })
 
-
-// let imgNameList=['batman.png','black-widow.png','captain-america.png','hulk.png','spider-man.png','wonder-woman.png']
-
-// let timer = 60
-// let timerIntervalID;
-// //make a obj of array with 12 empty item in it
-// let cardArray = new Array(12)
-// let lastSelectedCard = null
-// let moveCount = 0 
-
-// function flipAllcards(){
-//     cardArray.forEach((object)=>{
-//         object.card.classList.toggle('flip')
-
-//     })
-// }
-
-// //this function gets the list of image arrays and duplicate it 
-// function getImageNameList(array){
-//     let duplicateImageName = array.concat(array)
-// //shuffle the cards randomly every time the game starts
-//     duplicateImageName.sort(()=> Math.random() - .5)
-//     return duplicateImageName
-
-// }
-
-// function flipMissmatchCard(card1,card2){
-//     card1.card.classList.toggle('flip')
-//     card1.flipPermission = true
-
-//     card2.card.classList.toggle('flip')
-//     card2.flipPermission = true
-
-// }
-
-// function isFinished(){
-//     for(let i=0 ; i <12 ; i++){
-//         ///find one card that game is not finished
-//         if(cardArray[i].flipPermission === true)
-//           return false
-//     }
-//     return true
-
-// }
-
-
-// function checkFinished()
-
-
-
-
-
-
-// function prepareCards(){
-//     //Refactor my html with for loop
-//     let cardContainer = document.getElementById('card-container')
-//     //clear the board each time when push play game 
-//     cardContainer.innerHTML = ""
-
-//     let imageNames = getImageNameList(imgNameList)
-
-//     for(i=0 ; i< 12 ; i++){
-
-//         let gameCard = document.createElement('div')
-//         gameCard.classList.add('game-card')
-//         // gameCard.setAttribute('id',i)
-        
-//         let frontImage = document.createElement('img')
-//         frontImage.classList.add('card-front','card-style')
-//         let imageName = imageNames.pop()
-//         frontImage.setAttribute('src',`./img/${imageName}`)
-
-//         let backImage =document.createElement('img')
-//         backImage.classList.add('card-back','card-style')
-//         backImage.setAttribute('src','./img/general-assembly.png')
-
-//         gameCard.appendChild(frontImage)
-//         gameCard.appendChild(backImage)
-
-//         cardContainer.appendChild(gameCard)
-//         //with this obj I can access to the card and the permisssion for flip it
-//         cardArray[i] = {
-//             'card' : gameCard,
-//             'flipPermission' : true,
-//             'name' : imageName
-//         }
-
-//         gameCard.onclick = () =>{
-//             //This the current card that I picked
-//             // let cardID = parseInt(event.target.parentElement.id)
-//             // let currentCard = cardArray[cardID]
-//             let currentCard = cardArray[i]
-            
-//             //set condition for when I click on card then I couldn't click on it again
-//             if(currentCard.flipPermission === true){
-//                 //flip the card
-//                 currentCard.card.classList.toggle('flip')
-//                 currentCard.flipPermission = false
-               
-//                 //tell that the card is second card choose by player not the first card
-//                 if(lastSelectedCard !== null){
-//                     //check the matching cards
-//                     if(lastSelectedCard.name === currentCard.name){
-//                         //when two cards matched the thirdv card again will be our first card 
-//                         lastSelectedCard = null
-//                         //
-//                         if (isFinished())
-//                             alert('The game is finished')
-
-//                     }//the cards are not matched
-//                     else{
-//                         setTimeout(flipMissmatchCard,1000,lastSelectedCard,currentCard)
-//                         lastSelectedCard = null
-//                     }
-//                 }else{
-//                     //if this click is my first click
-//                     moveCount++;
-//                     lastSelectedCard = currentCard
-//                 }
-//                 document.getElementById('move-number').innerHTML = moveCount
-
-//         }
-
-
-//     }
-    
-//  }
-//     //when the bord cards make with for loop this func flip all cards and again after 5s it flip the cards again
-//     flipAllcards()
-//     setTimeout(flipAllcards,5000)
-
-
-// }
-// //this function count down the timer
-//  function timerCountDown(){
-//      timer--;
-//      document.getElementById('timer').innerHTML = timer
-//      //we need to clearInterval to set time till 0 
-//      if(timer === 0){
-//          clearInterval(timerIntervalID)
-//      }
-
-
-//  }
-// window.addEventListener('load',() =>{
-//     let playGameBtn =document.getElementById('play-game')
-//     playGameBtn.onclick = () => {
-//         prepareCards()
-//         timer = 60
-//         timerIntervalID = setInterval(timerCountDown,1000)
-        
-        
-//     }
-// })
 
 
 
